fa0f433bee029427de12ede160393736a0b53d5e
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2018-04-01 19:52:24 +0200

    Sending messages using websockets works. Adding missing jquery library.

--- games/consumers.py
+++ games/consumers.py
@@ -1,14 +1,24 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
+# A channel is a mailbox where messages can be sent to. Each channel has a name.
+# Anyone who has the name of a channel can send a message to the channel.
+
+# A group is a group of related channels. A group has a name.
+# Anyone who has the name of a group can add/remove a channel to the group by name and send a message to
+# all channels in the group. It is not possible to enumerate what channels are in a particular group.
+
 
 class GameConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        self.game_pk = self.scope['url_route']['kwargs']['pk_game']
+    def __init__(self, scope):
+        super().__init__(scope)
+        self.game_name = self.scope['url_route']['kwargs']['pk_game']
+        self.game_group_name = 'game_%s' % self.game_name
 
+    async def connect(self):
         # Join room group
         await self.channel_layer.group_add(
-            self.game_pk,
+            self.game_group_name,
             self.channel_name
         )
 
@@ -17,29 +27,33 @@ class GameConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         # Leave room group
         await self.channel_layer.group_discard(
-            self.game_pk,
+            self.game_group_name,
             self.channel_name
         )
 
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        message_type = text_data_json['type']
+        if message_type == 'hello_message':
+            print("Message type: ", message_type)
+            # Send message to room group
+            await self.channel_layer.group_send(
+                self.game_group_name, {
+                    'type': 'hello_message_broadcast',
+                    'message': text_data_json['message']
+                }
+            )
+        else:
+            print("Strange message type!")
 
-        # Send message to room group
-        await self.channel_layer.group_send(
-            self.game_pk,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    # Receive message from room group
-    async def chat_message(self, event):
+    # Receive message from room group (sent type <- game_message)
+    async def hello_message_broadcast(self, event):
         message = event['message']
+        print("Message: ", message)
 
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
-            'message': message
+            'type': 'hello_message_server',
+            'message': "Hello message from server."
         }))
--- games/routing.py
+++ games/routing.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from games import consumers
 
 websocket_urlpatterns = [
-    url(r'rooms/(?P<pk_room>\d+)/games/(?P<pk_game>\d+)$', consumers.GameConsumer),
+    url(r'ws/games/(?P<pk_game>\d+)$', consumers.GameConsumer),
 ]
