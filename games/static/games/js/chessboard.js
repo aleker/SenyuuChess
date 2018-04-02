ac37c3f2c191a2a2daf0e3a68842bbf106e2e67c
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2018-04-02 11:17:06 +0200

    Pieces displayed in proper size. Blurred images.

--- games/static/games/js/chessboard.js
+++ games/static/games/js/chessboard.js
@@ -20,6 +20,8 @@ RIP = 1;
 
 var chessCanvas = null;
 var ctx = null;
+var white_pieces = null;
+var black_pieces = null;
 
 document.addEventListener('DOMContentLoaded', function () {
     chessCanvas = document.getElementById("chess-canvas");
@@ -30,34 +32,30 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     BLOCK_SIZE = chessCanvas.height / NUMBER_OF_ROWS;
     drawBoard();
+    prepareImages();
     // draw();
+    // chessCanvas.addEventListener('click', board_click, false);
 
 }, false);
-var white_pieces = null;
-function draw(piecePositionsJson) {
-    // drawBoard();
-    // var chessPositions = getPiecePositions();
 
-    // Draw white pieces
+function prepareImages() {
+    // Prepare white pieces
     white_pieces = new Image();
-    // var path = '../../../static/games/img/SenyuuChess_white.png';
-    var path = DJANGO_IMAGE_URL + 'SenyuuChess_white.png';
-    white_pieces.src = path;
+    white_pieces.src = DJANGO_IMAGE_URL + 'SenyuuChess_white.png';
     // white_pieces.width = chessCanvas.width;
     white_pieces.onload = function() {
-        // IMG_BLOCK_SIZE = white_pieces.width / NUMBER_OF_COLS;
-        drawTeamOfPieces(piecePositionsJson.white, false);
+        IMG_BLOCK_SIZE = white_pieces.width / NUMBER_OF_COLS;
     };
 
-    // Draw black pieces
-    // var black_pieces = new Image();
-    // var path = DJANGO_STATIC_URL + '/img/SenyuuChess_black.png';
-    // black_pieces.src = path;
-    // black_pieces.onload = function() {
-    //     drawTeamOfPieces(piecePositionsJson.black, true);
-    // };
+    // Prepare black pieces
+    black_pieces = new Image();
+    black_pieces.src = DJANGO_IMAGE_URL + 'SenyuuChess_black.png';
+}
 
-    // chessCanvas.addEventListener('click', board_click, false);
+function draw(piecePositionsJson) {
+    // ctx.clearRect(0, 0, chessCanvas.width, chessCanvas.height);
+    drawTeamOfPieces(piecePositionsJson.white, false, true);
+    drawTeamOfPieces(piecePositionsJson.black, true, true);
 }
 
 function drawBoard() {
@@ -86,37 +84,52 @@ function drawField(rowNo, fieldNo) {
 function getBlockColour(rowNo, fieldNo) {
     var color;
     if (rowNo % 2)
-        color = (fieldNo % 2 ? BLOCK_COLOUR_WHITE : BLOCK_COLOUR_BLACK);
-    else
         color = (fieldNo % 2 ? BLOCK_COLOUR_BLACK : BLOCK_COLOUR_WHITE);
+    else
+        color = (fieldNo % 2 ? BLOCK_COLOUR_WHITE : BLOCK_COLOUR_BLACK);
     return color;
 }
 
-function drawTeamOfPieces(team, isBlackTeam) {
+function drawTeamOfPieces(team, isBlackTeam, isHappy) {
     var pieceNo;
     for (pieceNo = 0; pieceNo < team.length; pieceNo++) {
-        drawPiece(team[pieceNo], isBlackTeam);
+        drawPiece(team[pieceNo], isBlackTeam, isHappy);
     }
 }
 
-function drawPiece(curPiece, isBlackTeam) {
-    var imageCoords = getImageCoords(curPiece.piece, isBlackTeam);
+function drawPiece(curPiece, isBlackTeam, isHappy) {
+    var imageCoords = getImageCoords(curPiece.piece, isHappy);
 
     // Draw the piece onto the canvas
     // ctx.drawImage(white_pieces,
     //     imageCoords.x, imageCoords.y, BLOCK_SIZE, BLOCK_SIZE,
     //     curPiece.col * BLOCK_SIZE, curPiece.row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-    ctx.drawImage(white_pieces,
-        imageCoords.x, imageCoords.y, BLOCK_SIZE, BLOCK_SIZE,
+    // context.drawImage(
+        // img,
+        // x_gdzie zaczac_na_rysunku,
+        // y_gdzie_zaczac_na_rysunku,
+        // swidth_ile_z_szerokosci_obrazu,
+        // sheight_ile_z_wysokosci_obrazu,
+        // x_na_canvasie,
+        // y_na_canvasie,
+        // width_ile_ma_zajac_na_planszy,
+        // height_ile_ma_zajac_na_planszy);
+    if (isBlackTeam) {
+        ctx.drawImage(black_pieces,
+        imageCoords.x, imageCoords.y, IMG_BLOCK_SIZE, IMG_BLOCK_SIZE,
         curPiece.col * BLOCK_SIZE, curPiece.row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-    // TODO if black draw in reverse order
-
+    }
+    else {
+        ctx.drawImage(white_pieces,
+            imageCoords.x, imageCoords.y, IMG_BLOCK_SIZE, IMG_BLOCK_SIZE,
+            curPiece.col * BLOCK_SIZE, curPiece.row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+    }
 }
 
-function getImageCoords(pieceCode, isBlackTeam) {
+function getImageCoords(pieceCode, isHappy) {
     return {
-        "x": pieceCode * BLOCK_SIZE,
-        "y": (isBlackTeam ? 0 : BLOCK_SIZE)
+        "x": pieceCode * IMG_BLOCK_SIZE,
+        "y": (isHappy ? 0 : IMG_BLOCK_SIZE)
     };
 }
 
