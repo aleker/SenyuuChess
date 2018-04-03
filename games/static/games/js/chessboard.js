dd3481767dcc7314c745436d742e440299b49c02
aleker <aleksandra.kerebinska@student.put.poznan.pl>
2018-04-03 20:53:18 +0200

    Moving works. Pieces loaded after page refreshing.

--- games/static/games/js/chessboard.js
+++ games/static/games/js/chessboard.js
@@ -1,8 +1,6 @@
 // STYLE
 // board:
 NUMBER_OF_ROWS = 8;
-BLOCK_SIZE = null;
-IMG_BLOCK_SIZE = null;
 COLOUR_WHITE = 'white';
 COLOUR_BLACK = 'black';
 
@@ -12,7 +10,7 @@ blackImgName = 'SenyuuChess_black.png';
 
 // piece selection:
 SELECT_LINE_WIDTH = 3;
-HIGHLIGHT_COLOUR = 'blue';
+HIGHLIGHT_COLOUR = '#31add3';
 
 // PIECES
 PIECE_PAWN = 0;
@@ -27,146 +25,31 @@ PIECE_KING = 7;
 IN_PLAY = 0;
 RIP = 1;
 
-var chessCanvas = null;
-var selectCanvas = null;
-var ctx = null;
-var ctxSel = null;
-var white_pieces = null;
-var black_pieces = null;
-var currentPiecePositions = null;
+let setOfPieces = null;
+let chessboard = null;
 
 document.addEventListener('DOMContentLoaded', function () {
-    chessCanvas = document.getElementById("chess-canvas");
-    selectCanvas = document.getElementById("select-canvas");
-    ctx = chessCanvas.getContext("2d");
-    if (!ctx) {
-        alert("Canvas not supported!");
-        return;
-    }
-    ctxSel = selectCanvas.getContext("2d");
-    BLOCK_SIZE = chessCanvas.height / NUMBER_OF_ROWS;
-    drawBoard();
-    prepareImages();
-    selectCanvas.addEventListener('click', board_click, false);
+    chessboard = new Chessboard(document.getElementById("chess-canvas"), document.getElementById("select-canvas"));
+    setOfPieces = new SetOfPieces();
+    setOfPieces.white_pieces.onload = function () {
+        setOfPieces.IMG_BLOCK_SIZE = setOfPieces.white_pieces.width / NUMBER_OF_ROWS;
+    };
+    chessboard.draw();
+
+    chessboard.selectCanvas.addEventListener('click', function (ev) {
+        let clickCoordinates = computeClickCoordinates(ev);
+        let clickedField = chessboard.findField(clickCoordinates.x, clickCoordinates.y);
+
+        if (setOfPieces.selectedPiece == null)
+            processSelection(clickedField);
+        else
+            processMove(clickedField);
+    }, false);
 
 }, false);
 
-/****************
-* BOARD
-*****************/
-
-function drawBoard() {
-    ctx.clearRect(0, 0, chessCanvas.width, chessCanvas.height);
-    // Draw Rows
-    for(var rowNo = 0; rowNo < NUMBER_OF_ROWS; rowNo++) {
-        drawRow(rowNo);
-    }
-
-    // Draw chessboard border
-    ctx.lineWidth = 3;
-    ctx.strokeRect(0, 0, NUMBER_OF_ROWS * BLOCK_SIZE, NUMBER_OF_ROWS * BLOCK_SIZE);
-}
-
-function drawRow(rowNo) {
-    for(var fieldNo = 0; fieldNo < NUMBER_OF_ROWS; fieldNo++) {
-        drawField(rowNo, fieldNo);
-    }
-}
-
-function drawField(rowNo, fieldNo) {
-    ctx.fillStyle = getBlockColour(rowNo, fieldNo);
-    ctx.fillRect(rowNo * BLOCK_SIZE, fieldNo * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-    ctx.stroke();
-}
-
-function getBlockColour(rowNo, fieldNo) {
-    var color;
-    if (rowNo % 2)
-        color = (fieldNo % 2 ? COLOUR_BLACK : COLOUR_WHITE);
-    else
-        color = (fieldNo % 2 ? COLOUR_WHITE : COLOUR_BLACK);
-    return color;
-}
-
-/****************
-* PIECES
-*****************/
-
-function prepareImages() {
-    // Prepare white pieces
-    white_pieces = new Image();
-    white_pieces.src = DJANGO_IMAGE_URL + whiteImgName + '';
-    // white_pieces.width = chessCanvas.width;
-    white_pieces.onload = function() {
-        IMG_BLOCK_SIZE = white_pieces.width / NUMBER_OF_ROWS;
-    };
-
-    // Prepare black pieces
-    black_pieces = new Image();
-    black_pieces.src = DJANGO_IMAGE_URL + blackImgName + '';
-}
-
-function drawPieces() {
-    drawTeamOfPieces(currentPiecePositions.white, false, true);
-    drawTeamOfPieces(currentPiecePositions.black, true, true);
-}
-
-function updateCurrentPiecesPositions(newPiecePositionsJson) {
-    currentPiecePositions = newPiecePositionsJson;
-}
-
-function drawTeamOfPieces(team, isBlackTeam, isHappy) {
-    var pieceNo;
-    for (pieceNo = 0; pieceNo < team.length; pieceNo++) {
-        drawPiece(team[pieceNo], isBlackTeam, isHappy);
-    }
-}
-
-function drawPiece(curPiece, isBlackTeam, isHappy) {
-    var imageCoords = getImageCoords(curPiece.piece, isHappy);
-
-    // Draw the piece onto the canvas
-    // ctx.drawImage(white_pieces,
-    //     imageCoords.x, imageCoords.y, BLOCK_SIZE, BLOCK_SIZE,
-    //     curPiece.col * BLOCK_SIZE, curPiece.row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-    // context.drawImage(
-        // img,
-        // x_gdzie zaczac_na_rysunku,
-        // y_gdzie_zaczac_na_rysunku,
-        // swidth_ile_z_szerokosci_obrazu,
-        // sheight_ile_z_wysokosci_obrazu,
-        // x_na_canvasie,
-        // y_na_canvasie,
-        // width_ile_ma_zajac_na_planszy,
-        // height_ile_ma_zajac_na_planszy);
-    if (isBlackTeam) {
-        ctx.drawImage(black_pieces,
-        imageCoords.x, imageCoords.y, IMG_BLOCK_SIZE, IMG_BLOCK_SIZE,
-        curPiece.col * BLOCK_SIZE, curPiece.row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-    }
-    else {
-        ctx.drawImage(white_pieces,
-            imageCoords.x, imageCoords.y, IMG_BLOCK_SIZE, IMG_BLOCK_SIZE,
-            curPiece.col * BLOCK_SIZE, curPiece.row * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
-    }
-}
-
-function getImageCoords(pieceCode, isHappy) {
-    return {
-        "x": pieceCode * IMG_BLOCK_SIZE,
-        "y": (isHappy ? 0 : IMG_BLOCK_SIZE)
-    };
-}
-
-/****************
-* SELECT AND MOVE
-*****************/
-
-var selectedPiece = null;
-var currentTurn = COLOUR_WHITE;
-
-function board_click(ev) {
-    var x, y;
+function computeClickCoordinates(ev) {
+    let x, y;
     if (ev.pageX || ev.pageY) {
         x = ev.pageX;
         y = ev.pageY;
@@ -175,84 +58,26 @@ function board_click(ev) {
         x = ev.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
         y = ev.clientY + document.body.scrollTop + document.documentElement.scrollTop;
     }
-    x -= chessCanvas.offsetLeft;
-    y -= chessCanvas.offsetTop;
-
-    var clickedBlock = canvasToBlockNo(x, y);
-
-    if(selectedPiece == null) {
-        checkIfPieceClicked(clickedBlock);
-    }
-    else {
-        processMove(clickedBlock);
-    }
+    x -= chessboard.chessCanvas.offsetLeft;
+    y -= chessboard.chessCanvas.offsetTop;
+    return {"x": x, "y": y};
 }
 
-function canvasToBlockNo(pos_x, pos_y) {
-    var blockSize = (chessCanvas.clientWidth / NUMBER_OF_ROWS);
-    var column = parseInt(pos_x / blockSize);
-    var row = parseInt(pos_y / blockSize);
-    return {'col': column, 'row': row}
-}
-
-function checkIfPieceClicked(clickedBlock) {
-    var pieceAtBlock = getPieceAtBlock(clickedBlock, currentTurn);
-
-    if (pieceAtBlock !== null) {
-        selectPiece(pieceAtBlock);
+function processSelection(clickedField) {
+    const clickedPiece = setOfPieces.checkIfPieceClicked(clickedField);
+    if (clickedPiece !== false) {
+        chessboard.selectField(clickedPiece);
+        setOfPieces.selectedPiece = clickedPiece;
     }
-}
-
-function getPieceAtBlock(clickedBlock, teamColor) {
-    var pieceAtBlock = null;
-
-    if (teamColor === false) {
-        pieceAtBlock = getPieceAtBlock(clickedBlock, COLOUR_WHITE);
-        if (pieceAtBlock === null)
-            pieceAtBlock = getPieceAtBlock(clickedBlock, COLOUR_BLACK);
-    }
-    else {
-        var team = (teamColor === COLOUR_BLACK ?
-        currentPiecePositions.black : currentPiecePositions.white);
-
-        for (var iPieceCounter = 0; iPieceCounter < team.length; iPieceCounter++) {
-            var curPiece = team[iPieceCounter];
-            if (curPiece.status === IN_PLAY &&
-                curPiece.col === clickedBlock.col &&
-                curPiece.row === clickedBlock.row) {
-
-                pieceAtBlock = curPiece;
-                pieceAtBlock.id = iPieceCounter;
-                break;
-            }
-        }
-    }
-    return pieceAtBlock;
-}
-
-function selectPiece(pieceAtBlock) {
-    // TODO fix rectangle size
-    // Draw outline
-    ctxSel.lineWidth = SELECT_LINE_WIDTH;
-    ctxSel.strokeStyle = HIGHLIGHT_COLOUR;
-    ctxSel.strokeRect((pieceAtBlock.col * BLOCK_SIZE) + SELECT_LINE_WIDTH,
-        (pieceAtBlock.row * BLOCK_SIZE) + SELECT_LINE_WIDTH,
-        BLOCK_SIZE - (SELECT_LINE_WIDTH * 2),
-        BLOCK_SIZE - (SELECT_LINE_WIDTH * 2));
-
-    selectedPiece = pieceAtBlock;
-}
-
-function deselectPiece(context) {
-    context.clearRect(0, 0, selectCanvas.width, selectCanvas.height);
 }
 
 function processMove(clickedBlock) {
-    var pieceAtBlock = getPieceAtBlock(clickedBlock, false);
+    let pieceAtBlock = setOfPieces.getPieceAtBlock(clickedBlock, false);
 
     if (pieceAtBlock !== null) {
-        if (isMyAlly(pieceAtBlock.color)) {
-            changeSelectedPiece(pieceAtBlock);
+        if (setOfPieces.isMyAlly(pieceAtBlock.color)) {
+            console.log("my ally");
+            chessboard.changeSelectedPiece(pieceAtBlock);
             return;
         }
     }
@@ -260,88 +85,250 @@ function processMove(clickedBlock) {
 }
 
 function tryToMove(clickedBlock, enemyPiece) {
-    if (canSelectedMoveToBlock(selectedPiece, clickedBlock, enemyPiece) === true) {
+    if (SetOfPieces.isMovePermitted(setOfPieces.selectedPiece, clickedBlock, enemyPiece) === true) {
         movePiece(clickedBlock, enemyPiece);
     }
 }
 
-function isMyAlly(clickedPieceColor) {
-    return (clickedPieceColor === currentTurn)
-}
-
-function changeSelectedPiece(newPieceToSelect) {
-    deselectPiece(ctxSel);
-    selectPiece(newPieceToSelect)
-}
-
-function canSelectedMoveToBlock(selectedPiece, clickedBlock, enemyPiece)
-{
-    var bCanMove = false;
-
-    switch (selectedPiece.piece)
-    {
-        case PIECE_PAWN:
-            // TODO
-        break;
-
-        case PIECE_CASTLE_1 || PIECE_CASTLE_2:
-
-            // TODO
-
-        break;
-
-        case PIECE_ROUKE:
-
-            // TODO
-
-        break;
-
-        case PIECE_BISHOP_1 || PIECE_BISHOP_2:
-
-            // TODO
-
-        break;
-
-        case PIECE_QUEEN:
-
-            // TODO
-
-        break;
-
-        case PIECE_KING:
-
-            // TODO
-
-        break;
-    }
-
-    bCanMove = true;
-    return bCanMove;
-}
-
 function movePiece(clickedBlock, enemyPiece) {
     // Clear the block in the original position
-    drawField(selectedPiece.col, selectedPiece.row);
+    chessboard.drawField(setOfPieces.selectedPiece.col, setOfPieces.selectedPiece.row);
 
-    var teamColor = (currentTurn === COLOUR_WHITE ? 'white' : 'black');
-    var oppositeColor = (currentTurn !== COLOUR_WHITE ? 'white' : 'black');
+    let teamColor = setOfPieces.currentTurn;
+    let oppositeColor = (setOfPieces.currentTurn !== COLOUR_WHITE ? COLOUR_WHITE : COLOUR_BLACK);
 
     // REMOVE ENEMY
     if (enemyPiece !== null) {
         // Clear beaten piece
-        drawField(enemyPiece.col, enemyPiece.row);
-        currentPiecePositions[oppositeColor][enemyPiece.id].status = RIP;
+        chessboard.drawField(enemyPiece.col, enemyPiece.row);
+        setOfPieces.currentPiecePositions[oppositeColor][enemyPiece.id].status = RIP;
     }
 
     // DRAW PIECE IN NEW POSITION
-    currentPiecePositions[teamColor][selectedPiece.id].col = clickedBlock.col;
-    currentPiecePositions[teamColor][selectedPiece.id].row = clickedBlock.row;
-    drawPiece(selectedPiece, (currentTurn === COLOUR_BLACK), true);
-    deselectPiece(ctxSel);
+    setOfPieces.currentPiecePositions[teamColor][setOfPieces.selectedPiece.id].col = clickedBlock.col;
+    setOfPieces.currentPiecePositions[teamColor][setOfPieces.selectedPiece.id].row = clickedBlock.row;
+    setOfPieces.drawPiece(setOfPieces.selectedPiece, (setOfPieces.currentTurn === COLOUR_BLACK), true);
+    chessboard.deselectField();
 
     // CLEAR TURN AND SELECTED PIECE
-    currentTurn = (currentTurn === COLOUR_WHITE ? COLOUR_BLACK : COLOUR_WHITE);
-    selectedPiece = null;
+    setOfPieces.currentTurn = (setOfPieces.currentTurn === COLOUR_WHITE ? COLOUR_BLACK : COLOUR_WHITE);
+    setOfPieces.selectedPiece = null;
+}
+
+/****************
+* CHESSBOARD
+*****************/
+
+class Chessboard {
+    constructor(chessCanvas, selectCanvas) {
+        this.chessCanvas = chessCanvas;
+        this.ctx = this.chessCanvas.getContext("2d");
+        this.selectCanvas = selectCanvas;
+        this.ctxSel = this.selectCanvas.getContext("2d");
+        this.BLOCK_SIZE = chessCanvas.height / NUMBER_OF_ROWS;
+    }
+
+    draw() {
+        this.ctx.clearRect(0, 0, this.chessCanvas.width, this.chessCanvas.height);
+        // Draw Rows
+        for(let rowNo = 0; rowNo < NUMBER_OF_ROWS; rowNo++) {
+            this.drawRow(rowNo);
+        }
+        // Draw chessboard border
+        this.ctx.lineWidth = 3;
+        this.ctx.strokeRect(0, 0, NUMBER_OF_ROWS * this.BLOCK_SIZE, NUMBER_OF_ROWS * this.BLOCK_SIZE);
+    };
+
+    drawRow(rowNo) {
+        for(let fieldNo = 0; fieldNo < NUMBER_OF_ROWS; fieldNo++) {
+            this.drawField(rowNo, fieldNo);
+        }
+    };
+
+    drawField(rowNo, fieldNo) {
+        this.ctx.fillStyle = Chessboard.getBlockColour(rowNo, fieldNo);
+        this.ctx.fillRect(rowNo * this.BLOCK_SIZE, fieldNo * this.BLOCK_SIZE, this.BLOCK_SIZE, this.BLOCK_SIZE);
+        this.ctx.stroke();
+    };
+
+    static getBlockColour(rowNo, fieldNo) {
+        let color;
+        if (rowNo % 2)
+            color = (fieldNo % 2 ? COLOUR_BLACK : COLOUR_WHITE);
+        else
+            color = (fieldNo % 2 ? COLOUR_WHITE : COLOUR_BLACK);
+        return color;
+    };
+
+    findField(pos_x, pos_y) {
+        let blockSize = (this.chessCanvas.clientWidth / NUMBER_OF_ROWS);
+        let column = parseInt(pos_x / blockSize);
+        let row = parseInt(pos_y / blockSize);
+        return {'col': column, 'row': row}
+    }
+
+    selectField(pieceAtBlock) {
+        // Draw outline
+        this.ctxSel.lineWidth = SELECT_LINE_WIDTH;
+        this.ctxSel.strokeStyle = HIGHLIGHT_COLOUR;
+        this.ctxSel.strokeRect((pieceAtBlock.col * this.BLOCK_SIZE) + SELECT_LINE_WIDTH,
+            (pieceAtBlock.row * this.BLOCK_SIZE) + SELECT_LINE_WIDTH,
+            this.BLOCK_SIZE - (SELECT_LINE_WIDTH * 2),
+            this.BLOCK_SIZE - (SELECT_LINE_WIDTH * 2));
+    }
+
+    deselectField() {
+        this.ctxSel.clearRect(0, 0, this.selectCanvas.width, this.selectCanvas.height);
+    }
+
+    changeSelectedPiece(newPieceToSelect) {
+        this.deselectField();
+        this.selectField(newPieceToSelect);
+    }
+}
+
+/****************
+* PIECES
+*****************/
+
+class SetOfPieces {
+    constructor() {
+        // white pieces
+        let white_pieces = new Image();
+        white_pieces.src = DJANGO_IMAGE_URL + whiteImgName + '';
+        this.white_pieces = white_pieces;
+        // black pieces
+        let black_pieces = new Image();
+        black_pieces.src = DJANGO_IMAGE_URL + blackImgName + '';
+        this.black_pieces = black_pieces;
+
+        this.IMG_BLOCK_SIZE = 70;
+        this.currentPiecePositions = null;
+        this.selectedPiece = null;
+        this.currentTurn = COLOUR_WHITE;
+    };
+
+    updateCurrentPiecesPositions(newPiecePositionsJson) {
+        this.currentPiecePositions = newPiecePositionsJson;
+    };
+
+    drawPieces() {
+        this.drawTeamOfPieces(this.currentPiecePositions.white, false, true);
+        this.drawTeamOfPieces(this.currentPiecePositions.black, true, true);
+    };
+
+    drawTeamOfPieces(team, isBlackTeam, isHappy) {
+        for (let pieceNo = 0; pieceNo < team.length; pieceNo++) {
+            this.drawPiece(team[pieceNo], isBlackTeam, isHappy);
+        }
+    }
+
+    drawPiece(curPiece, isBlackTeam, isHappy) {
+        let imageCoords = this.getImageCoords(curPiece.piece, isHappy);
+        if (isBlackTeam) {
+            chessboard.ctx.drawImage(this.black_pieces,
+                imageCoords.x, imageCoords.y, this.IMG_BLOCK_SIZE, this.IMG_BLOCK_SIZE,
+                curPiece.col * chessboard.BLOCK_SIZE, curPiece.row * chessboard.BLOCK_SIZE,
+                chessboard.BLOCK_SIZE, chessboard.BLOCK_SIZE);
+        }
+        else {
+            chessboard.ctx.drawImage(this.white_pieces,
+                imageCoords.x, imageCoords.y, this.IMG_BLOCK_SIZE, this.IMG_BLOCK_SIZE,
+                curPiece.col * chessboard.BLOCK_SIZE, curPiece.row * chessboard.BLOCK_SIZE,
+                chessboard.BLOCK_SIZE, chessboard.BLOCK_SIZE);
+        }
+    };
+
+    getImageCoords(pieceCode, isHappy) {
+        return {
+            "x": pieceCode * this.IMG_BLOCK_SIZE,
+            "y": (isHappy ? 0 : this.IMG_BLOCK_SIZE)
+        };
+    };
+
+    /****************
+    * SELECT AND MOVE
+    *****************/
+
+    checkIfPieceClicked(clickedField) {
+        let pieceOnField = this.getPieceAtBlock(clickedField, this.currentTurn);
+        if (pieceOnField !== null) return pieceOnField;
+        else return false;
+    };
+
+    getPieceAtBlock(clickedBlock, teamColor) {
+        let pieceAtBlock = null;
+
+        if (teamColor === false) {
+            pieceAtBlock = this.getPieceAtBlock(clickedBlock, COLOUR_WHITE);
+            if (pieceAtBlock === null)
+                pieceAtBlock = this.getPieceAtBlock(clickedBlock, COLOUR_BLACK);
+        }
+        else {
+            let team = (teamColor === COLOUR_BLACK ?
+            this.currentPiecePositions.black : this.currentPiecePositions.white);
+
+            for (let iPieceCounter = 0; iPieceCounter < team.length; iPieceCounter++) {
+                let curPiece = team[iPieceCounter];
+                if (curPiece.status === IN_PLAY &&
+                    curPiece.col === clickedBlock.col &&
+                    curPiece.row === clickedBlock.row) {
+
+                    pieceAtBlock = curPiece;
+                    pieceAtBlock.id = iPieceCounter;
+                    break;
+                }
+            }
+        }
+        return pieceAtBlock;
+    };
+
+    isMyAlly(clickedPieceColor) {
+        return (clickedPieceColor === this.currentTurn)
+    };
+
+    static isMovePermitted(selectedPiece, clickedBlock, enemyPiece) {
+        let bCanMove = false;
+
+        switch (selectedPiece.piece) {
+            case PIECE_PAWN:
+                // TODO
+            break;
+
+            case PIECE_CASTLE_1 || PIECE_CASTLE_2:
+
+                // TODO
+
+            break;
+
+            case PIECE_ROUKE:
+
+                // TODO
+
+            break;
+
+            case PIECE_BISHOP_1 || PIECE_BISHOP_2:
+
+                // TODO
+
+            break;
+
+            case PIECE_QUEEN:
+
+                // TODO
+
+            break;
+
+            case PIECE_KING:
+
+                // TODO
+
+            break;
+        }
+
+        bCanMove = true;
+        return bCanMove;
+    }
 }
 
 
